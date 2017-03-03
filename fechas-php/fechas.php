0944dbf1985c6dc152682dbe7e631d224be0bd37
Hiram Guerrero <hiramg90@gmail.com>
2017-03-02 21:39:23 -0600

    Subiendo practica moment.js

---
+++ fechas-php/fechas.php
@@ -0,0 +1,7 @@
+<?php
+
+//Establecemos la zona horaria:
+date_default_timezone_set('America/Mexico_City');
+
+//Imprimir fecha y hora actual (inglés): Sunday 25th of December 2016 07:03:45 PM
+echo date('l jS \of F Y h:i:s A');
