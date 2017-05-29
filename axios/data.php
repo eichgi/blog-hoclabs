c3e0e37283232d19eaebf50ff0a89114d6cbb7d6
Hiram G <hiramg90@gmail.com>
2017-05-29 16:55:37 -0500

    Actualizando practica de Axios

--- axios/data.php
+++ axios/data.php
@@ -1,6 +1,13 @@
 <?php
 
-sleep(2);
+$input = file_get_contents('php://input');
+
+//var_dump($input);exit;
+$_PATCH = json_decode($input);
+
+var_dump($_PATCH);
+//sleep(2);
+//echo json_encode($_POST);
 $data = [
     'Persona' => [
         'nombre' => 'Hiram',
@@ -10,4 +17,4 @@ $data = [
     'Lenguajes' => ['Javascript', 'PHP', 'Java']
 ];
 
-echo json_encode($data);
+//echo json_encode($data);
