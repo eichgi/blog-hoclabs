83a012044290565232b3d5aba993a1eb483dd7a9
Hiram G <hiramg90@gmail.com>
2017-05-29 21:15:04 -0500

    Actualizando ejercicio Axios

--- axios/data.php
+++ axios/data.php
@@ -1,20 +1,18 @@
 <?php
 
-$input = file_get_contents('php://input');
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
-//var_dump($input);exit;
-$_PATCH = json_decode($input);
 
-var_dump($_PATCH);
-//sleep(2);
-//echo json_encode($_POST);
-$data = [
-    'Persona' => [
-        'nombre' => 'Hiram',
-        'edad' => 26,
-        'profesion' => 'Desarrollador'
-    ],
-    'Lenguajes' => ['Javascript', 'PHP', 'Java']
-];
 
-//echo json_encode($data);
+} else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $datos = [
+        'Persona' => [
+            'nombre' => 'Hiram',
+            'edad' => 26,
+            'profesion' => 'Desarrollador'
+        ],
+        'Lenguajes' => ['Javascript', 'PHP', 'Java']
+    ];
+
+    echo json_encode($datos);
+}
